85e0831cbb59c3ed5e5b0b520ec17768803b3d11
thedocbwarren <jedi.master@augmentedjs.com>
2017-11-26 17:35:32 -0800

    a small hint

--- list.js
+++ list.js
@@ -4,9 +4,19 @@ const View = require("./view.js");
 class List extends View {
   constructor(name, el, list) { //pass in list instead of template
     super(name, el, ""); //no need to pass template you will remove and change. Use ""
+    // hist : if
     this._list = list;
   };
 
+  /*
+   * just a hint :)
+   *
+   * add(item) {
+   *   this._list.push(item);
+   *   this._refresh();
+   * };
+   *
+   */
   add() {
     list(['apples','orange']);
     console.log(list);
@@ -19,6 +29,18 @@ class List extends View {
   get list() {
     return this._list;
   };
+
+  /* hint 2 :)
+   *
+   * _refresh() {
+   *   // refresh the list with an iterator.  as in simular to you createList function but to _template
+   *   this._template = "<ul>";
+   *   for (i = 0; i < this._list.length; i++) {
+   *     this._template += `<li>${this._list[i]}`;
+   *   }
+   *   this._template += "</ul>";
+   * };
+   */
 
   // createList() {
   //
